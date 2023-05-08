e6159cc2f57b0cb3cc9a018205327e0fce79f3e6
c5du <97643279+c5du@users.noreply.github.com>
2023-05-07 23:13:54 -0700

    Update explore.js (2)

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,7 +8,7 @@ function init() {
   let timeoutID;
   const synth = window.speechSynthesis;
   const btn = document.querySelector('button');
-  const voiceSelect = document.querySelector('select');
+  const voiceSelect = document.getElementById('voice-select');
   var utterence;
   btn.addEventListener("click", speak);
   utterence.addEventListener("start", moveMouth);
@@ -17,19 +17,27 @@ function init() {
   function populateVoiceList(){
     voices = synth.getVoices();
     for(let i = 0; i < voices.length; i++){
-      const option = document.createElement("option");
+      /*const option = document.createElement("option");
       option.textContent = '${voices[i].name} (${voices[i].lang})';
       if(voices[i].default){
         option.textContent += " - DEFAULT";
       }
       option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);*/
+      var option = document.createElement('option');
+      const optionText = document.createTextNode(voices[i].name + " - " + voices[i].lang);
+      option.appendChild(optionText);
+      //let newOption = new Option(voices[i].lang, voices[i].name);
+      
+      option.setAttribute("data-lang", voices[i].lang);
       option.setAttribute("data-name", voices[i].name);
+      //voiceSelect.add(newOption,undefined);
       voiceSelect.appendChild(option);
     }
   }
   populateVoiceList();
-  if(speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = populateVoiceList;
+  if(synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = populateVoiceList;
   }
   function speak(){
     clearTimeout(timeoutID);
